ff9021d665d50e9fa2d8b695333e360f22c1f9be
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2023-12-13 20:25:00 +0100

    p23/f12

--- src/features/posts/use-cases/post.service.get.post.comments.usecase.ts
+++ src/features/posts/use-cases/post.service.get.post.comments.usecase.ts
@@ -1,9 +1,10 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CommentInfo } from "../entities/post.controller.get.comment";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CommentsRepoService } from "src/repo/comments/comments.repo.service";
 import { LikeForCommentRepoService } from "src/repo/likes/commentLikes/likes.for.comment.repo.service";
 import { CommentRepoEntity } from "src/repo/comments/entities/commen.repo.entity";
+import { PostsRepoService } from "src/repo/posts/posts.repo.service";
 
 export class PostServiceGetPostCommentsCommand {
 	constructor(
@@ -20,10 +21,14 @@ export class PostServiceGetPostCommentsCommand {
 @CommandHandler(PostServiceGetPostCommentsCommand)
 export class PostServiceGetPostCommentsUseCase implements ICommandHandler<PostServiceGetPostCommentsCommand, {count: number, comments: CommentInfo[]}>{
 
-	constructor(private commentRepo: CommentsRepoService) {}
+	constructor(private commentRepo: CommentsRepoService, private postRepo: PostsRepoService) {}
 
 	async execute(command: PostServiceGetPostCommentsCommand): Promise<{count: number;comments: CommentInfo[]}> {
+		let post = await this.postRepo.ReadById(command.postId);
 		
+		if(!post)
+			throw new NotFoundException("Post not found");
+
 		let countAndComments = await this.commentRepo.ReadAndCountManyForCertainPost(command.postId, command.sortBy, command.sortDirection, command.userId, command.skip, command.limit)
 
 		return countAndComments;
